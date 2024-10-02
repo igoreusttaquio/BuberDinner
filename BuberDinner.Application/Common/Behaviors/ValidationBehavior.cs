01dde2cdf5b92c4bae82ee091440bb47fcfd3119
Igor E. Silva <igoreusttaquio@gmail.com>
2024-10-01 21:53:02 -0300

    Add pipeline behavior os mediatr.

---
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,18 @@
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Common.Behaviors;
+
+public class ValidateRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request, RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next, CancellationToken cancellationToken)
+    {
+        // before handler
+        var result = await next();
+
+        // after handler
+        return result;
+    }
+}
