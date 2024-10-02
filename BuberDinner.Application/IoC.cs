2dc6c9cab9be3662b57bf660842edd96e3cbb140
Igor E. Silva <igoreusttaquio@gmail.com>
2024-10-01 21:53:11 -0300

    Add fluent validation

--- BuberDinner.Application/IoC.cs
+++ BuberDinner.Application/IoC.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuberDinner.Application.Authentication.Commands.Register;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using ErrorOr;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Application;
 
@@ -7,6 +12,7 @@ public static class IoC
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly((typeof(IoC).Assembly)));
+        services.AddScoped<IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>, ValidateRegisterCommandBehavior>();
         return services;
     }
 }
