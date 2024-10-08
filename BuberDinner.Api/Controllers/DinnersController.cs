a1d63b4eac7bf64625e4e8050168df7719ac55c6
Igor E. Silva <igoreusttaquio@gmail.com>
2024-10-08 20:16:12 -0300

    Dinners Controller

---
+++ BuberDinner.Api/Controllers/DinnersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DinnersController : ApiController
+{
+    [HttpGet]
+    public IActionResult ListDinners()
+    {
+        return Ok(Array.Empty<string>());
+    }
+}
